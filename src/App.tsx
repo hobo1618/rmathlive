631377044b32b326599be534b61fafbe626d1a42
Hobo Spaceship <hobdenw@gmail.com>
2022-11-02 12:49:37 +0100

    move makeshared vkb out of component

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,29 @@ import MathView, { MathViewRef } from "react-math-view";
 import { makeSharedVirtualKeyboard } from "mathlive";
 import { HIGH_SCHOOL_KEYBOARD, HIGH_SCHOOL_KEYBOARD_LAYER } from "./keyboard";
 
+makeSharedVirtualKeyboard();
+
 function App() {
   const ref1 = useRef<MathViewRef>(null);
   const ref2 = useRef<MathViewRef>(null);
-  const [mf1, setMf1] = useState("")
-  const [mf2, setMf2] = useState("")
-  makeSharedVirtualKeyboard();
+  const [mf1, setMf1] = useState("");
+  const [mf2, setMf2] = useState("");
 
   useEffect(() => {
     ref1.current?.setOptions({
-      "customVirtualKeyboardLayers": HIGH_SCHOOL_KEYBOARD_LAYER,
-      "customVirtualKeyboards": HIGH_SCHOOL_KEYBOARD,
-      "virtualKeyboards": "high-school-keyboard"
-    })
-  },[])
+      customVirtualKeyboardLayers: HIGH_SCHOOL_KEYBOARD_LAYER,
+      customVirtualKeyboards: HIGH_SCHOOL_KEYBOARD,
+      virtualKeyboards: "high-school-keyboard",
+    });
+
+    // add this if you want both keyboards to be the same.
+    // ref2.current?.setOptions({
+    //   customVirtualKeyboardLayers: HIGH_SCHOOL_KEYBOARD_LAYER,
+    //   customVirtualKeyboards: HIGH_SCHOOL_KEYBOARD,
+    //   virtualKeyboards: "high-school-keyboard",
+    // });
+
+  }, []);
 
   return (
     <div>
